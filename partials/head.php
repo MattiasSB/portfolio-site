63607cc27b12c254613d9c267451ef5aacfa42f5
mattiasbellan <m.bellan500@gmail.com>
2023-02-22 15:43:34 -0800

    updated to sass

--- partials/head.php
+++ partials/head.php
@@ -4,6 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image" href="/img/Logo.svg">
     <title>Portfolio Home - Mattias Bellan</title>
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/scss/reset.css">
+    <link rel="stylesheet" href="/dist/css/styles.min.css">
 </head>
