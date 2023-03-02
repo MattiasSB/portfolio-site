5c2eab349fcfe99bf7f53ba9bfa176fc39851f2b
mattiasbellan <m.bellan500@gmail.com>
2023-03-02 13:39:23 -0800

    adjudted menu and intro section + pixel - rem

--- about.php
+++ about.php
@@ -1,18 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-    <!--Flickity for side scrolls-->
-    <?php 
-    @require_once("init.php");
-    ?>
-
 <?php 
+@require_once("init.php");
 @require("partials/head.php");
 ?>
 <body>
     <?php 
     @require("partials/header.php");
     ?>
-    
+    <div class="banner__image"></div>
 
     <script src="/scripts/flickity.pkgd.min.js"></script>
     <script src="/scripts/menu.js"></script>
