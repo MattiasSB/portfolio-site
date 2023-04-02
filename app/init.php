1b9b301be58a55ee2610db94628672ce9eefc04f
mattiasbellan <m.bellan500@gmail.com>
2023-04-01 20:00:59 -0700

    changed init back website root

--- app/init.php
+++ app/init.php
@@ -1,7 +1,7 @@
 <?php 
 
-    define('WWW_ROOT', 'http://localhost:8888');
-    // define('WWW_ROOT', 'https://mattiasbellan.ca');
+    // define('WWW_ROOT', 'http://localhost:8888');
+    define('WWW_ROOT', 'https://mattiasbellan.ca');
     define('PROJECT_ROOT', dirname(__DIR__, 1));
 
     require(__DIR__ . "/classes/cards__class.php");
