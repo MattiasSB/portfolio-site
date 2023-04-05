415b2498d61991ace9e33ff9f0733dab0e06abde
mattiasbellan <m.bellan500@gmail.com>
2023-04-05 00:58:00 -0700

    changed css font import to link

--- public/partials/head.php
+++ public/partials/head.php
@@ -6,7 +6,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php echo $metaDescription?>"> 
+    <meta name="description" content="<?php echo $metaDescription?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&family=Staatliches&display=swap" rel="stylesheet">
     <link rel="icon" type="image/svg" href="img/Logo.svg">
     <title>Portfolio <?php echo $titleTag?> - Mattias Bellan</title>
     <link rel="stylesheet" href="dist/css/styles.min.css?v=1.6">
